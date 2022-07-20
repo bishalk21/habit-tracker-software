cceae387590dc562e4d54effba63f3d695c682b0
Your Name <you@example.com>
2022-07-21 09:14:34 +1000

    new Schema connected with mongooese

--- api/src/model/UserSchema.js
+++ api/src/model/UserSchema.js
@@ -1,3 +1,31 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", UserSchema);
