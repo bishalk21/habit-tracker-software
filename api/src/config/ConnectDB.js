7076172100da555a9a6c207273a77afc1422d51c
Your Name <you@example.com>
2022-07-21 08:29:53 +1000

    database configured

--- api/src/config/ConnectDB.js
+++ api/src/config/ConnectDB.js
@@ -2,5 +2,13 @@ import React from "react";
 import mongoose from "mongoose";
 
 export const ConnectDB = () => {
-  return <div>ConnectDB</div>;
+  try {
+    const MONGO_CLIENT = "mongodb://localhost/habit-tracker";
+    const con = mongoose.connect(MONGO_CLIENT);
+    if (con) {
+      console.log("Connected to Mongo DB");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
