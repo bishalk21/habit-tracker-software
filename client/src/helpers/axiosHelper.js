2ed5f079bcb3bdc1b612ce4948987b8faf1e6dda
Your Name <you@example.com>
2022-07-23 14:23:39 +1000

    imported toastify, toastcontainer in client, and posNewUser upon successful registration wth of axios

--- client/src/helpers/axiosHelper.js
+++ client/src/helpers/axiosHelper.js
@@ -3,8 +3,13 @@ import axios from "axios";
 const rootUrl = "http://localhost:8000";
 const UserP = rootUrl + "/api/v1/user/";
 
+// postNewUser is for registering a new user
 const postNewUser = async (user) => {
+  // user is an object
   try {
+    const response = await axios.post(UserP, user);
+    // console.log(response);
+    return response.data; // data is actually the user object
   } catch (error) {
     return {
       status: "error",
